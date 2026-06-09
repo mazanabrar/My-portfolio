9fa39fd77af5613febc4fd6216df895ef83ae8ef
ICT-Division <ict-division@ict-mac-pc.local>
2026-06-09 15:59:09 +0500

    fix: force update all case sensitive files for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/navbar'; // Match your exact file name (navbar or Navbar)
 import Hero from './components/Hero';
-import About from './components/About'; // <--- Fixed: Capital 'A' to match About.jsx
+import About from './components/about'// <--- Fixed: Capital 'A' to match About.jsx
 import Skills from './components/skills'; // Match your exact file name (skills or Skills)
 import Projects from './components/projects'; // Match your exact file name (projects or Projects)
 import Contact from './components/contact'; // Match your exact file name (contact or Contact)
